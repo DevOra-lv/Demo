a2ea0f51d4a04ec615b895d53c111a180f38f697
Artūrs Palamarčuks <arturspalamarcuks@inbox.lv>
2025-08-18 19:50:51 +0300

    initail project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+
+  return (
+    <div className='text-2xl bg-black'>
+      <p className='text-amber-100'>REACT</p>
+      <p className='text-amber-300'>+</p>
+      <p className='text-amber-500'>TAILWIND</p>
+      <p className='text-amber-700'>=</p>
+      <p className='text-amber-900'>DEMO SHOWCASE</p>
+    </div>
+  )
+}
+
+export default App
